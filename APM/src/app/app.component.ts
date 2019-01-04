eeb4447a6b57eae7fa566904b5820f9d9049c837
andrzejadczak <andrzejadczak@gmail.com>
2019-01-04 15:44:05 +0100

    Initial commit

---
+++ APM/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+// template: `` multiline bot in ''
+@Component({
+  selector: 'pm-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  pageTitle: string = 'Product Management';
+}
---
+++ APM/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    WelcomeComponent,
+    ConvertToSpacesPipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
